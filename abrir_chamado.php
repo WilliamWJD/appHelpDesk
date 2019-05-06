86d5a532fe3c6af90d311c8a99aed5a9dbbb321c
William <informatica01@COOPER.local>
2019-05-06 14:53:27 -0300

    Navegação entre as paginas

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -61,11 +61,11 @@
 
                     <div class="row mt-5">
                       <div class="col-6">
-                        <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+                        <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
                       </div>
 
                       <div class="col-6">
-                        <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
+                        <a class="btn btn-lg btn-info btn-block" href="home.php">Abrir</a>
                       </div>
                     </div>
                   </form>
